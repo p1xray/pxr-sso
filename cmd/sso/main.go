ac64f5ebe193b170059a8378b0499df564869137
Mihail Solovev <youoliver@mail.ru>
2025-04-16 19:37:41 +0300

    feat: added config

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"love-signal-sso/internal/config"
+)
 
 func main() {
+	cfg := config.MustLoad()
+
 	fmt.Println("Hello from sso app!")
+	fmt.Printf("with config: %v", cfg)
 }
