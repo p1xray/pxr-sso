9ed7013ae4fff916977cb8ba598a3d287b074f33
Mihail Solovev <youoliver@mail.ru>
2025-04-28 22:10:21 +0300

    feat: implemented a method for getting a user by username

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -3,8 +3,10 @@ package sqlite
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"pxr-sso/internal/domain"
+	"pxr-sso/internal/storage"
 )
 
 // Storage provides access to sqlite storage.
@@ -26,12 +28,55 @@ func New(storagePath string) (*Storage, error) {
 
 // UserByUsername returns a user from the storage by their username.
 func (s *Storage) UserByUsername(ctx context.Context, username string) (domain.User, error) {
+	const op = "sqlite.SaveUser"
 
+	stmt, err := s.db.PrepareContext(ctx,
+		`select
+    		u.id,
+    		u.username,
+    		u.password_hash,
+    		u.fio,
+    		u.date_of_birth,
+    		u.gender,
+    		u.avatar_file__key,
+    		u.deleted,
+    		u.created_at,
+    		u.updated_at
+		from users u
+		where u.username = ?;`)
+	if err != nil {
+		return domain.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	row := stmt.QueryRowContext(ctx, username)
+
+	var user domain.User
+	err = row.Scan(
+		&user.ID,
+		&user.Username,
+		&user.PasswordHash,
+		&user.FIO,
+		&user.DateOfBirth,
+		&user.Gender,
+		&user.AvatarFileKey,
+		&user.Deleted,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return domain.User{}, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+
+		return domain.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, err
 }
 
 // UserPermissions returns the user's permissions from the storage.
 func (s *Storage) UserPermissions(ctx context.Context, userID int64) ([]domain.Permission, error) {
-	
+
 }
 
 // UserClient returns the user's client from the storage by code.
