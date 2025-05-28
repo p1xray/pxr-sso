4d43d547cb7254fcd98fa87d87d86d3b4dbfb5dd
Mihail Solovev <youoliver@mail.ru>
2025-05-28 21:07:37 +0300

    feat: finished token validator

--- pkg/jwt/validator/option.go
+++ pkg/jwt/validator/option.go
@@ -1,9 +1,17 @@
 package validator
 
+import "time"
+
 type Option func(*Validator)
 
 func WithCustomClaims(f func() CustomClaims) Option {
 	return func(v *Validator) {
 		v.customClaims = f
+	}
+}
+
+func WithAllowedClockSkew(d time.Duration) Option {
+	return func(v *Validator) {
+		v.allowedClockSkew = d
 	}
 }
