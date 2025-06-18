72058db2c01a19573e765178b8e5409f4adfad71
Mihail Solovev <youoliver@mail.ru>
2025-06-18 18:50:59 +0300

    fix: fixed auth header check

--- pkg/jwt/extractor.go
+++ pkg/jwt/extractor.go
@@ -27,7 +27,7 @@ func AuthHeaderTokenExtractor(r *http.Request) (string, error) {
 	}
 
 	authHeaderParts := strings.Fields(authHeader)
-	if len(authHeaderParts) != 2 || strings.ToLower(authHeaderParts[0]) != bearerPrefix {
+	if len(authHeaderParts) != 2 || strings.ToLower(authHeaderParts[0]) != strings.ToLower(bearerPrefix) {
 		return "", ErrInvalidHeaderFormat
 	}
 
